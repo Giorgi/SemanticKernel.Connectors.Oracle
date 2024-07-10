1c344025428a0f2e2ca8b091eeab673191f6d5c8
Giorgi <giorgi@giorgi.dev>
2024-07-11 00:22:24 +0400

    Created initial OracleMemoryStore class

---
+++ SemanticKernel.Connectors.Oracle/OracleMemoryStore.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.SemanticKernel.Memory;
+
+namespace SemanticKernel.Connectors.Oracle
+{
+    [Experimental("SKEXP0001")]
+    public class OracleMemoryStore : IMemoryStore
+    {
+        public Task CreateCollectionAsync(string collectionName, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<string> GetCollectionsAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DoesCollectionExistAsync(string collectionName, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCollectionAsync(string collectionName, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpsertAsync(string collectionName, MemoryRecord record,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<string> UpsertBatchAsync(string collectionName, IEnumerable<MemoryRecord> records,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MemoryRecord?> GetAsync(string collectionName, string key, bool withEmbedding = false,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<MemoryRecord> GetBatchAsync(string collectionName, IEnumerable<string> keys, bool withEmbeddings = false,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(string collectionName, string key, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveBatchAsync(string collectionName, IEnumerable<string> keys,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<(MemoryRecord, double)> GetNearestMatchesAsync(string collectionName, ReadOnlyMemory<float> embedding, int limit,
+            double minRelevanceScore = 0, bool withEmbeddings = false,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(MemoryRecord, double)?> GetNearestMatchAsync(string collectionName, ReadOnlyMemory<float> embedding, double minRelevanceScore = 0,
+            bool withEmbedding = false, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
